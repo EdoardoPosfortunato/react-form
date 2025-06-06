a5d8dca133ed2ee376c0c0f3738a69cd953b23b1
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-06-06 15:31:52 +0200

    feat: milestone 1 - lista titoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import mangas from './data/manga.js'
 
 function App() {
 
+
+
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
+      <div className='container'>
+        <h1 className='my-4'>MangaList</h1>
+        <ul className='list-group'>
+          {mangas.map((currManga, index) =>
+            <li 
+            key={currManga.id}
+            className='card'>{currManga.title}</li>
+          )}
+        </ul>
       </div>
     </>
   )
---
+++ src/data/manga.js
@@ -0,0 +1,34 @@
+const mangas = [
+  {
+    id: 1,
+    title: "One Piece",
+    author: "Eiichiro Oda",
+    plot: "Pirati, tesori, avventura, amicizia."
+  },
+  {
+    id: 2,
+    title: "Naruto",
+    author: "Masashi Kishimoto",
+    plot: "Ninja, sogni, legami, combattimenti."
+  },
+  {
+    id: 3,
+    title: "Attack on Titan",
+    author: "Hajime Isayama",
+    plot: "Giganti, umanità, mistero, sopravvivenza."
+  },
+  {
+    id: 4,
+    title: "Death Note",
+    author: "Tsugumi Ohba, Takeshi Obata",
+    plot: "Quaderno mortale, giustizia, ingegno."
+  },
+  {
+    id: 5,
+    title: "Demon Slayer",
+    author: "Koyoharu Gotouge",
+    plot: "Demoni, vendetta, famiglia, spada."
+  }
+];
+
+export default mangas;
